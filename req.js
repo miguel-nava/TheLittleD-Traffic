f2d2cc858c0ee2e76a3bfd2cebf097124f0a24b7
Tapas Kapadia <tapaskapadia@Tapass-MacBook-Pro.local>
2019-03-02 15:22:10 -0600

    adding kyle's changes as well as my db stuff

---
+++ req.js
@@ -0,0 +1,25 @@
+const http = require('http')
+const options = {
+  hostname: 'localhost',
+  port: 3001,
+  path: '/miguel',
+  method: 'GET'
+}
+
+var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+  
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+  
+  // write data to request body
+  req.write('data\n');
+  req.write('data\n');
+  req.end();
--- rest-server.py
+++ rest-server.py
@@ -3,8 +3,12 @@ from __future__ import print_function
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask import render_template, redirect
 from requests.utils import quote
-
+from twilio.rest import Client
+from collections import defaultdict
+import schedule
+import time
 #import MySQLdb
+import pymysql
 import sys
 import csv
 import requests 
@@ -12,26 +16,83 @@ import json
 app = Flask(__name__, static_url_path="")
 
 
-USERNAME = 'root'
-PASSWORD = 'blahblahblah'
-DB_NAME = 'crime'
-#Insert host url from RDS
+USERNAME = 'tapas'
+PASSWORD = 'password'
+DB_NAME = 'opendata'
+# ##
+# # Insert host url from RDS
 # connect = MySQLdb.connect (	host = "",
-#                         	user = USERNAME,
-#                         	passwd = PASSWORD,
-#                         	db = DB_NAME, 
-# 				port = 3306
-# 						)
+#                          	user = USERNAME,
+#                          	passwd = PASSWORD,
+#                          	db = DB_NAME, 
+#  				            port = 3306)
 
 # conn  = connect.cursor()
 
-
+con = pymysql.connect(host="", user=USERNAME, password=PASSWORD, db=DB_NAME, cursorclass=pymysql.cursors.
+                                   DictCursor)
+cur = con.cursor()
 
 @app.route('/', methods=['GET'])
 def getHome():
     return render_template('index.html')
 
 
+@app.route('/miguel')
+def miguel():
+    return 'Miguel Called this route'
+
+@app.route('/tapas')
+def tapas():
+    cur.execute("SELECT phonenumber, fname FROM contactpeeps LIMIT 50")
+    result = cur.fetchall()
+    print(result)
+    return "result"
+
+@app.route('/kyle')
+def kyle():
+    numbers_dict = defaultdict(lambda: "")
+    numbers_dict["14048191425"] = "Tapas Kapadia"
+    message_string = "you suck ill eat ur pea brain"
+    notify_events(numbers_dict, message_string)
+    return 'sent'
+
+
+# numbers_dict = defaultdict(lambda: "")
+# numbers_dict["+17176391921"] = "Dalton"
+# numbers_dict["+15183547556"] = "Kyle"
+# numbers_dict["+17138359559"] = "Pea Brain"
+# numbers_dict["+17138359559"] = "Tapas Kapadia"
+# message_string = "you suck ill eat ur pea brain"
+
+# (list, string)
+def notify_events(numbers, message):
+    for number, name in numbers.items():
+        
+        # Twilio Authentication
+        account_sid = "ACd1710704a119c7266c11e227a40408fb" 
+        auth_token = "c1a7bde509f696defe6c8836eabee114"
+        sms_string = "Hi " + name + "!!!, " + message
+        
+        client = Client(account_sid, auth_token)
+        media_url = "https://tse1.mm.bing.net/th?id=OIP.cV7nTqlOSi88l2-5o_IdewHaIy&pid=Api"
+        response = client.messages.create(
+            to= number, 
+            from_= "+17176198944",
+            body= sms_string,
+            media_url= media_url # if you need to attach multimedia to your message, else remove this parameter.
+        )
+        print(response.sid)
+        
+#notify_events(numbers_dict, message_string)
+        
+'''    
+schedule.every().day.at("08:00").do(notify_events)
+   
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
+'''
 
 
 
@@ -180,29 +241,10 @@ def getShootings():
 @app.route('/reps', methods=['GET', 'POST'])
 def getRepresentativeInformation(): 
     if request.method == 'POST': 
-        searchLocation = request.form['location'] 
-        params = {"address":searchLocation, "levels":"administrativeArea1"}
-        civicsURL = "https://www.googleapis.com/civicinfo/v2/representatives?key="
-        response = requests.get(civicsURL, params=params).json()
-        try:
-            officials = response['officials']
-        except: 
-            return render_template('representatives.html')
-        returnData = []
-        city = officials[0]['address'][0]['city']
-        state = officials[0]['address'][0]['state']
-        zipcode = officials[0]['address'][0]['zip']
-        line1 = officials[0]['address'][0]['line1']
-        line1 = quote(line1, safe='')
-        for official in officials: 
-            officialData = {};
-            officialData['name'] = official['name']
-            officialData['party'] = official['party']
-            officialData['website'] = official['urls'][0]
-            officialData['phone'] = official['phones'][0]
-            returnData.append(officialData)
-        mapsURL = "https://www.google.com/maps/embed/v1/place?key==" + line1 + "," + city + "+" + state + "," + zipcode
-        return render_template('representatives.html', reps=returnData, map=mapsURL)
+        pnumber = request.form['pnumber'] 
+        fname = request.form['fname'] 
+        print(pnumber,fname)
+        return render_template('representatives.html')
     else: 
         return render_template('representatives.html') 
 
